e0ab1f59f4a1a716d4a4ef773bfbc80149e6f2b7
Xiaochuan Wang <nkwdwxc@gmail.com>
2013-07-10 14:20:14 +0000

    step 1

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var INPUT_FILE = 'index.html';
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = new Buffer(fs.readFileSync(INPUT_FILE), "utf-8");
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
